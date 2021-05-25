619950cd3bab514c650bce42666168e60dda1941
galymax <werkhowski@gmail.com>
2021-05-25 12:20:15 +0200

    Updates

---
+++ precache-manifest.ab3f984985ce5401035e26a2ae448c18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98eb342a0ab52dc6ab144ea59f7320f6",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/index.html"
+  },
+  {
+    "revision": "9ebf488c3fcb9776ec40",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2a16e060f517c54bbaba",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/css/main.ac0c4aaf.chunk.css"
+  },
+  {
+    "revision": "9ebf488c3fcb9776ec40",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/js/2.7241a1e1.chunk.js"
+  },
+  {
+    "revision": "a78a2484d0ca5f7cd9f3bea7f7b9abc4",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/js/2.7241a1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a16e060f517c54bbaba",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/js/main.15524b7e.chunk.js"
+  },
+  {
+    "revision": "1a87e6e4332883b7fbfe",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/js/runtime-main.32e1ef86.js"
+  },
+  {
+    "revision": "aa94107606296e3e6989f9ee04a88ab7",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/media/gif.aa941076.gif"
+  },
+  {
+    "revision": "97aa399600907928f822032066a69b0f",
+    "url": "/galymax.github.io/e04-record-shop-frontend-galymax/static/media/vinyl.97aa3996.jpeg"
+  }
+]);
